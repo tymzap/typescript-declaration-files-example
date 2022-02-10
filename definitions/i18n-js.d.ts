cc5066c9283d0cce3ee86f8192f3be717c39b466
Tymoteusz Zapała <tymoteusz.zapala@miquido.com>
2022-02-10 11:14:05 +0100

    Fix re-export in overriding external library types example

--- definitions/i18n-js.d.ts
+++ definitions/i18n-js.d.ts
@@ -3,6 +3,13 @@
 // We can modify library definition to any extent in this way, for example re-export
 // only chosen declarations.
 declare module 'i18n-js' {
-  export * from 'i18n-js/index';
-  export let locale: 'pl' | 'en';
+  type Locale = 'en' | 'pl';
+  // We must use absolute path to library type definition to avoid circular dependency
+  export {
+    t,
+    reset,
+    getFullScope
+  } from 'i18n-js/index';
+  export let locale: Locale;
+  export const currentLocale: () => Locale;
 }
